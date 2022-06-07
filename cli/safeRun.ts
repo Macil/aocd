03a8b95e5485394e88436d6dcf89b5e53ab0c8bf
Chris Cowan <agentme49@gmail.com>
2022-06-07 02:03:01 -0700

    use std http server

--- cli/safeRun.ts
+++ cli/safeRun.ts
@@ -1,5 +1,6 @@
 import { getAocd } from "../mod.ts";
 import { basicAuth } from "https://deno.land/x/basic_auth@v1.0.2/mod.ts";
+import { Server } from "https://deno.land/std@0.142.0/http/server.ts";
 
 export interface SafeRunOptions {
   scriptArg: string;
@@ -10,82 +11,65 @@ export interface SafeRunOptions {
 export async function safeRun(
   options: SafeRunOptions,
 ): Promise<Deno.ProcessStatus> {
-  const apiServer = Deno.listen({ port: 0, hostname: "localhost" });
-  if (apiServer.addr.transport !== "tcp") {
-    throw new Error("Should not happen: wrong transport mode");
-  }
   const abortController = new AbortController();
-  abortController.signal.addEventListener("abort", () => {
-    apiServer.close();
+
+  const password = crypto.randomUUID();
+
+  const server = new Server({
+    port: 0,
+    hostname: "localhost",
+    async handler(request, _connInfo) {
+      const errResponse = basicAuth(request, "sandbox", {
+        sandbox: password,
+      });
+      if (errResponse) {
+        return errResponse;
+      }
+      const url = new URL(request.url);
+      if (url.pathname === "/getInput") {
+        const year = Number(url.searchParams.get("year"));
+        const day = Number(url.searchParams.get("day"));
+        const input = await getAocd().getInput(year, day);
+        return new Response(input, {
+          headers: {
+            "Content-Type": "text/plain",
+          },
+          status: 200,
+        });
+      } else if (url.pathname === "/submit") {
+        if (!options.submit) {
+          return new Response("Forbidden", { status: 403 });
+        }
+        const body = await request.json();
+        const { year, day, part, solution } = body;
+        if (
+          typeof year !== "number" || typeof day !== "number" ||
+          typeof part !== "number" || typeof solution !== "number"
+        ) {
+          return new Response("Invalid body", { status: 400 });
+        }
+        const correct = await getAocd().submit(year, day, part, solution);
+        const responseBody = { correct };
+        return new Response(JSON.stringify(responseBody), {
+          headers: {
+            "Content-Type": "application/json",
+          },
+          status: 200,
+        });
+      } else {
+        return new Response("Not found", { status: 404 });
+      }
+    },
   });
+  abortController.signal.addEventListener("abort", () => {
+    server.close();
+  }, { once: true });
   try {
-    const { addr } = apiServer;
-
-    const password = crypto.randomUUID();
-
-    const runServer = async () => {
-      async function respond(request: Request): Promise<Response> {
-        try {
-          const errResponse = basicAuth(request, "sandbox", {
-            sandbox: password,
-          });
-          if (errResponse) {
-            return errResponse;
-          }
-          const url = new URL(request.url);
-          if (url.pathname === "/getInput") {
-            const year = Number(url.searchParams.get("year"));
-            const day = Number(url.searchParams.get("day"));
-            const input = await getAocd().getInput(year, day);
-            return new Response(input, {
-              headers: {
-                "Content-Type": "text/plain",
-              },
-              status: 200,
-            });
-          } else if (url.pathname === "/submit") {
-            if (!options.submit) {
-              return new Response("Forbidden", { status: 403 });
-            }
-            const body = await request.json();
-            const { year, day, part, solution } = body;
-            if (
-              typeof year !== "number" || typeof day !== "number" ||
-              typeof part !== "number" || typeof solution !== "number"
-            ) {
-              return new Response("Invalid body", { status: 400 });
-            }
-            const correct = await getAocd().submit(year, day, part, solution);
-            const responseBody = { correct };
-            return new Response(JSON.stringify(responseBody), {
-              headers: {
-                "Content-Type": "application/json",
-              },
-              status: 200,
-            });
-          } else {
-            return new Response("Not found", { status: 404 });
-          }
-        } catch (err) {
-          console.error("Error while serving request", err);
-          return new Response("Internal Server Error", { status: 500 });
-        }
-      }
-
-      async function serveHttp(conn: Deno.Conn) {
-        // This "upgrades" a network connection into an HTTP connection.
-        const httpConn = Deno.serveHttp(conn);
-        // Each request sent over the HTTP connection will be yielded as an async
-        // iterator from the HTTP connection.
-        for await (const requestEvent of httpConn) {
-          requestEvent.respondWith(respond(requestEvent.request));
-        }
-      }
-
-      for await (const conn of apiServer) {
-        serveHttp(conn);
-      }
-    };
+    const serverPromise = server.listenAndServe();
+    const addr = server.addrs[0];
+    if (addr.transport !== "tcp") {
+      throw new Error("Should not happen: wrong transport mode");
+    }
 
     const runScript = async () => {
       const apiDomain = `localhost:${addr.port}`;
@@ -126,7 +110,7 @@ export async function safeRun(
       return status;
     };
 
-    const [, status] = await Promise.all([runServer(), runScript()]);
+    const [, status] = await Promise.all([serverPromise, runScript()]);
     return status;
   } finally {
     abortController.abort();
