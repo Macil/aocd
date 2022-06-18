7223ab80fc6035eb81686e5cf461ce4934695a6b
Chris Cowan <agentme49@gmail.com>
2022-06-17 20:21:10 -0700

    allow manually configuring Aocd

--- mod.ts
+++ mod.ts
@@ -9,32 +9,66 @@ export { version } from "./version.ts";
 
 let singleton: Aocd | undefined;
 
+/**
+ * Main entrance into this module. Returns an automatically configured
+ * Aocd instance by default. The instance may be manually configured by
+ * using {@link setAocd} before this.
+ */
 export function getAocd(): Aocd {
-  if (!singleton) {
-    const parsedArgs = parse(Deno.args, {
-      boolean: ["s", "submit"],
-      string: ["aocd-api-addr"],
-    });
-    const submit = Boolean(parsedArgs.s || parsedArgs.submit);
-    if (parsedArgs["aocd-api-addr"]) {
-      singleton = new SafeRunAocd({ submit }, parsedArgs["aocd-api-addr"]);
-    } else {
-      singleton = new DefaultAocd({ submit });
-    }
+  const existingAocd = getAocdIfSet();
+  if (existingAocd) {
+    return existingAocd;
+  }
+  const parsedArgs = parse(Deno.args, {
+    boolean: ["s", "submit"],
+    string: ["aocd-api-addr"],
+  });
+  const submit = Boolean(parsedArgs.s || parsedArgs.submit);
+  if (parsedArgs["aocd-api-addr"]) {
+    singleton = new SafeRunAocd({ submit }, parsedArgs["aocd-api-addr"]);
+  } else {
+    singleton = new DefaultAocd({ submit });
   }
   return singleton;
 }
 
 /**
- * Get the current DefaultAocd singleton. Throws an error if called
+ * Get the current Aocd instance if it's set.
+ * This is mainly for internal use but is exposed to help debugging.
+ * Generally the {@link getAocd} function should be used instead of this.
+ */
+export function getAocdIfSet(): Aocd | undefined {
+  return singleton;
+}
+
+/**
+ * Allow the Aocd singleton to be set if it hasn't been set yet.
+ * Use this if you want to manually configure where Aocd gets its data
+ * from.
+ */
+export function setAocd(aocd: Aocd) {
+  if (singleton) {
+    throw new Error("Aocd instance is already set");
+  }
+  singleton = aocd;
+}
+
+/**
+ * Get the current {@link DefaultAocd} singleton. Throws an error if called
  * while `aocd safe-run` is being used.
+ * The function {@link getAocd} is recommended over this unless you
+ * specifically need functionality that doesn't work in safe-run mode.
  */
 export function getDefaultAocd(): DefaultAocd {
   const aocd = getAocd();
   if (aocd instanceof DefaultAocd) {
     return aocd;
   } else {
-    throw new Error("getDefaultAocd() is disallowed inside safe-run");
+    if (aocd instanceof SafeRunAocd) {
+      throw new Error("getDefaultAocd() is disallowed inside safe-run");
+    } else {
+      throw new Error("Aocd instance is not DefaultAocd instance");
+    }
   }
 }
 
