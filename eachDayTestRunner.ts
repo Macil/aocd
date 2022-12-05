fd6ea8f8afbe8e47a08a785544c0dd65581a3550
Chris Cowan <agentme49@gmail.com>
2022-12-05 04:36:28 -0800

    make project template test task run all regular Deno tests too

--- eachDayTestRunner.ts
+++ eachDayTestRunner.ts
@@ -1,6 +1,6 @@
-// This module does the equivalent of running `deno test day_*.ts "$@"`.
-// This module exists just because `deno task` does not support glob
-// expressions yet.
+import { walk } from "https://deno.land/std@0.167.0/fs/walk.ts";
+
+// This module does the equivalent of running `deno test REGULAR_TESTS... day_*.ts "$@"`.
 
 const dayScriptFilenames: string[] = [];
 for await (const entry of Deno.readDir(".")) {
@@ -8,15 +8,29 @@ for await (const entry of Deno.readDir(".")) {
     dayScriptFilenames.push(entry.name);
   }
 }
+// Natural sort the day script filenames so they're in order by day number.
 dayScriptFilenames.sort((a, b) => {
   const aNum = Number(/\d+/.exec(a)![0]);
   const bNum = Number(/\d+/.exec(b)![0]);
   return aNum - bNum;
 });
+
+const regularTestFilenames: string[] = [];
+for await (
+  const item of walk(".", {
+    includeDirs: false,
+    match: [/(?:^|[\._])test\.(?:tsx?|jsx?|[cm][tj]s)$/],
+  })
+) {
+  regularTestFilenames.push(item.path);
+}
+regularTestFilenames.sort();
+
 const p = Deno.run({
   cmd: [
     "deno",
     "test",
+    ...regularTestFilenames,
     ...dayScriptFilenames,
     ...Deno.args,
   ],
