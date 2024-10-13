0d039948a657ff50345330372317ea3197dc878b
Chris Cowan <agentme49@gmail.com>
2024-10-12 17:20:13 -0700

    better error message if start command is used before init command

--- cli/start.ts
+++ cli/start.ts
@@ -1,7 +1,18 @@
 import { createDayScript } from "./_createDayScript.ts";
 
 export async function start(day: number) {
-  const config = JSON.parse(await Deno.readTextFile(".aocdrc.json"));
+  let configText: string;
+  try {
+    configText = await Deno.readTextFile(".aocdrc.json");
+  } catch (err) {
+    if (err instanceof Deno.errors.NotFound) {
+      throw new Error(
+        `.aocdrc.json file not found. The "start" command must be used in a directory created with the "init" command.`,
+      );
+    }
+    throw err;
+  }
+  const config = JSON.parse(configText);
   const year = config.year;
   if (typeof year !== "number") {
     throw new Error("Invalid year in config .aocdrc.json");
